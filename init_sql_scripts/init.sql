75447f1cb17ceb2078cf8b2e47957e2aba4dccab
= <=>
2020-07-25 11:05:39 +0300

    Add Poland

--- init_sql_scripts/init.sql
+++ init_sql_scripts/init.sql
@@ -8,3 +8,4 @@ insert into country_and_capitals(country, capital) values ('China', 'Beijing');
 insert into country_and_capitals(country, capital) values ('Bhutan', 'Thimpu');
 insert into country_and_capitals(country, capital) values ('Russia', 'Moscow');
 insert into country_and_capitals(country, capital) values ('Bulgaria', 'Sofia');
+insert into country_and_capitals(country, capital) values ('Poland', 'Warsaw');
